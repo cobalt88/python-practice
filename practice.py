b698bdf0b16abccf641318d982aabcac5822aca0
cobalt88 <vincentteune@gmail.com>
2022-09-07 19:13:50 -0400

    9.7.22
    playing with the date library

--- practice.py
+++ practice.py
@@ -1,10 +1,24 @@
-print("Hello world!")
-print(5+5)
-print(5-5)
-print(5*5)
-print(5/5)
-print(5%5)
-print(5**5)
-print(5//5)
-print(5==5)
-print(5!=5)
+# print("Hello world!")
+# print(5+5)
+# print(5-5)
+# print(5*5)
+# print(5/5)
+# print(5%5)
+# print(5**5)
+# print(5//5)
+# print(5==5)
+# print(5!=5)
+from datetime import date
+
+fruits = ["apple", "banana", "cherry"]
+nums = [1, 2, 3, 4, 5]
+
+for x in range(25): 
+  if x%2 == 0:
+    print(x, "even")
+  else:
+    print(x, "odd")
+
+print("done")
+
+print(date.today())

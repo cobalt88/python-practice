a3e0c657500e26a5e6a724128c787b7b4ab6fb5a
cobalt88 <vincentteune@gmail.com>
2022-11-26 13:53:24 -0500

    add in blockchain validation

--- acdemind/blockchain.py
+++ acdemind/blockchain.py
@@ -28,17 +28,34 @@ def add_transaction(transaction_amount, last_transaction=[1]):
 def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
     # Get the user input, transform it from a string to a float and store it in user_input
-    user_input = float(input('Your transaction amount please: '))
+    user_input = float(input('Enter transaction amount: '))
     return user_input
 
 def get_user_choice():
-    user_input = input('Your choice')
+    user_input = input('Your choice: ')
     return user_input
 
 def print_blockchain_elements():
     for block in blockchain:
         print('outputting block')
         print(block)
+
+#this function will check the validity of the blockchain by comparing the current first element of the current chain against the entirety of the previous block. 
+def verify_chain():
+    block_index = 0
+    is_valid = True
+
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid
 
 # Get the first transaction input and add the value to the blockchain
 tx_amount = get_transaction_value()
@@ -47,10 +64,12 @@ add_transaction(tx_amount)
 # user interface loop, while true the user is prompted to input either a new transaction value or print out the current blockchain 
 while True:
     #options for the user
-    print('Please Choose')
+    print('Please Choose an option from below: \n')
     print('1: Add a new transaction value')
     print('2: Output current blockchain')
-    print('0: Exit')
+    print('3: Edit or manipulate the blockchain')
+    print('0: Exit \n')
+
     #function to record input from the user
     user_choice = get_user_choice()
     #conditional statement for user interface questions
@@ -59,11 +78,18 @@ while True:
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
+    elif user_choice == '3':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     elif user_choice == '0':
         print('Thank you for using Blockchain.py!')
         break
     else: 
         print('Invalid input. Please choose an option from the list.')
+    # verify the chain
+    if not verify_chain():
+        print('Invalid blockchain!')
+        break
         
     
     # Output the blockchain list to the console
--- acdemind/conditionals.py
+++ acdemind/conditionals.py
@@ -1,2 +1,15 @@
 user = "Vincent"
+age = 30
 
+if age >= 20 and age <= 40:
+  print('Age is between 20 and 40')
+elif age > 20:
+  print('Age is less than 20')
+elif age > 40:
+  print('Age is greater than 40')
+
+if user == 'Vincent' and age > 25 or age < 30:
+  print('Vincent is either older than 29 or younger than 20')
+
+if user == 'Vincent' and (age > 25 or age < 30):
+  print('Vincent is either older than 29 or younger than 20 - 2nd version of conditional statement')

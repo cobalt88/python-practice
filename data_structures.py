7375a5342c19059b42f12152bc78e15640c9f657
cobalt88 <vincentteune@gmail.com>
2022-12-12 18:32:05 -0500

    test

---
+++ data_structures.py
@@ -0,0 +1,15 @@
+# Lists are mutable, ordered, allow duplicate data and allow multiple data types
+
+example_list = ['Element 1', 'Element 2', 3, 4.0]
+
+# a set is similar to a list, it is mutable, multiple data types are allowed, however a set does not allow duplicate data
+
+example_set = {'element 1', 2, 3.0, 'Element 4'}
+
+# A tuple is immutable. Once created it cannot be edited. They are ordered, allow duplicate data and allow mixed data types. 
+
+example_tuple = ("element 1", 2, 3.5, 'Element 4')
+
+# Dictionaries are similar to objects, but not the same. They do require key value pairs => "Key":"Value" => are unordered maps, keys cannot be duplicated however values can be, and mixed data types are allowed for values.
+
+example_dictionary = {'name':'your name here', 'age': 30, 'favorite foods': ['pizza', 'apples', 'pasta']}
